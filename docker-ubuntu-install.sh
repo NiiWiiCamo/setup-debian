398ee511afdd55b0cc66a74643b7e72683f804c4
NiiWiiCamo <mail@nilsblume.de>
2022-12-13 15:20:57 +0100

    Update docker-ubuntu-install.sh
    apt install -y

--- docker-ubuntu-install.sh
+++ docker-ubuntu-install.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -13,6 +9,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
